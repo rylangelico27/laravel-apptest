3b271879bd1abcc5769d1b9e004c47e9371b24fa
rylangelico27 <rylanangelico.tioco.cics@ust.edu.ph>
2023-10-18 22:03:41 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Laravel App</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+    </nav>
+
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Contact Us</h1>
+    <!-- Add your contact page content here -->
+@endsection
